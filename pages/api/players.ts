99707cd27bbd83ff935a6b3cd2295fe143c1c29b
vanak <ven2kaze@gmail.com>
2023-10-12 18:35:01 +0200

    upsert on db

--- pages/api/players.ts
+++ pages/api/players.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "@/src/db/prisma";
-import { z } from "zod";
-import { getTodayDate } from "@/src/utils/date";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '@/src/db/prisma';
+import { z } from 'zod';
+import { getTodayDate } from '@/src/utils/date';
 
 type Data = {
   name: string;
@@ -21,18 +21,39 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const body = req.body;
     const safeData = TodayPlayersScheme.parse(body);
 
+    const today = getTodayDate();
     for (let player of safeData) {
-      const attendance = {
-        date: getTodayDate(),
-        isPresent: player.attendance,
-        playerId: player.id,
-      };
-      await prisma.attendance.create({ data: attendance });
+      const attendance = await prisma.attendance.findFirst({
+        where: {
+          playerId: player.id,
+          date: today,
+        },
+      });
+      if (attendance) {
+        console.log('---- Update on db ----');
+        await prisma.attendance.update({
+          where: {
+            id: attendance.id,
+          },
+          data: {
+            isPresent: player.attendance,
+          },
+        });
+      } else {
+        console.log('---- Create on db ----');
+        await prisma.attendance.create({
+          data: {
+            date: today,
+            playerId: player.id,
+            isPresent: player.attendance,
+          },
+        });
+      }
     }
-    res.status(200).json({ name: "John Doe" });
+    res.status(200).json({ name: 'John Doe' });
   }
 }
